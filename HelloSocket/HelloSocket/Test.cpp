0bbbcb77e6436ef820813941c5ade6226aed6dcd
havenow <715667426@qq.com>
2018-11-17 16:43:37 +0800

    socket在windwos平台下的启动和关闭

--- HelloSocket/HelloSocket/Test.cpp
+++ HelloSocket/HelloSocket/Test.cpp
@@ -1,5 +1,20 @@
 
+#define  WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <WinSock2.h>
+
 int main(int argc, char** argv)
 {
+	WORD ver = MAKEWORD(2, 2);
+	WSADATA dat;
+	WSAStartup(ver, &dat);
+
+	///
+
+
+	///
+
+	WSACleanup();
 	return 0;
 }
