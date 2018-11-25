be14faf76ba6388b8e92d54cab871332e4e9fdc2
havenow <715667426@qq.com>
2018-11-25 19:33:08 +0800

    完善代码

--- HelloSocket/EasyTcpClient/client.cpp
+++ HelloSocket/EasyTcpClient/client.cpp
@@ -1,9 +1,8 @@
 
-#define WIN32_LEAN_AND_MEAN
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
-
 #ifdef _WIN32
+	#define WIN32_LEAN_AND_MEAN
+	#define _WINSOCK_DEPRECATED_NO_WARNINGS
+	#define _CRT_SECURE_NO_WARNINGS
 	#include <windows.h>
 	#include <WinSock2.h>
 #else
