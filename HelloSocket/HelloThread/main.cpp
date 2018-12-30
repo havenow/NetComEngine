7464ed0c28a9104c9ae1a177f1a0e016216fc839
havenow <715667426@qq.com>
2018-12-30 17:02:57 +0800

    多线程-传递参数给入口函数，创建线程数组

--- HelloSocket/HelloThread/main.cpp
+++ HelloSocket/HelloThread/main.cpp
@@ -2,17 +2,24 @@
 #include <thread>
 using namespace std;
 
-void workFun()
+void workFun(int index)
 {
 	for (int n = 0; n < 4; n++)
-		cout << "Hello, other thread." << endl;
+		cout << index << "Hello, other thread." << endl;
 }
 
 int main(int argc, char* argv[])
 {
-	std::thread t(workFun);
-	t.detach();
-	//t.join();
+	std::thread t[3];
+	for (int n = 0; n < 3; n++)
+	{
+		t[n] = std::thread(workFun, n);
+	}
+	for (int n = 0; n < 3; n++)
+	{
+		t[n].join();
+		//t[n].detach();
+	}
 	for (int n = 0; n < 4; n++)
 		cout << "Hello, main thread." << endl;
 	while (true)
